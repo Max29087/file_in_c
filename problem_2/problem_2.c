9d4785d78fdf4705f63e99fd581d7400e4c69b60
Max29087 <topusorker087@gmai.com>
2024-05-01 21:10:11 +0600

    Add new data

---
+++ problem_2/problem_2.c
@@ -0,0 +1,30 @@
+/*
+    How to write or read something on file ,.
+*/
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    char str1[1000];
+    printf("\n---Input---\n=>: ");
+    fgets(str1, 1000, stdin);
+    str1[strcspn(str1, "\n")] = '\0';
+    int len =  strlen(str1);
+    FILE *test;
+    test = fopen("Problem_2.txt", "w");
+    if (test == NULL)
+    {
+        printf("File dose not exist\n");
+    }
+    else
+    {
+        printf("File is opene\n");
+        for (int i = 0; i <len; i++)
+        {
+            fputc(str1[i],test);
+        }
+        printf("File is written successfully.\n");
+        fclose(test);
+    }
+    return 0;
+}
